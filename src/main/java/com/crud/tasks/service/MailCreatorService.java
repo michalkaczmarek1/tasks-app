d50cae86a9a8dcac4d6de8d1eff7de672b719058
Michal Kaczmarek <mi123456ka@gmail.com>
2020-12-23 00:02:29 +0100

    Customize template email

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,7 +1,6 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +9,6 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 @Service
-@Getter
 public class MailCreatorService {
 
     @Value("${info.company.name}")
@@ -39,7 +37,7 @@ public class MailCreatorService {
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbye_message", "Dziekujemy " + adminConfig.getAdminName() + " za skorzystanie z naszego serwisu! Miłego dnia");
-        context.setVariable("company_details", companyName + ", " + companyEmail + ", " + companyPhone +"<br>" + companyGoal);
+        context.setVariable("company_details", companyName + ", " + companyEmail + ", tel." + companyPhone +"\n" + companyGoal);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
