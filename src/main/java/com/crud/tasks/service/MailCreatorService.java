4b20ad5efcfcca00626c51663cee0cb8f6a197ee
Michal Kaczmarek <mi123456ka@gmail.com>
2020-12-22 23:57:05 +0100

    Customize template email

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,14 +1,29 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 @Service
+@Getter
 public class MailCreatorService {
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -24,6 +39,7 @@ public class MailCreatorService {
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbye_message", "Dziekujemy " + adminConfig.getAdminName() + " za skorzystanie z naszego serwisu! Miłego dnia");
+        context.setVariable("company_details", companyName + ", " + companyEmail + ", " + companyPhone +"<br>" + companyGoal);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
